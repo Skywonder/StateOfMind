5706157367c374294ff2bfd6d954f0dd94481c35
Skywonder <kuanpinc@ics.uci.edu>
2016-05-11 12:42:42 -0700

    Update (Kuan)

---
+++ Assets/Scripts/TriggerZone.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerZone : MonoBehaviour {
+
+	// Use this for initialization
+    public GameObject item; //<-this is the item that will appear at target location
+    public Vector3 targetLocation;
+    public float spawntime = 5f;
+
+
+	// Update is called once per frame
+	void Update () {
+	    
+
+
+
+	}
+
+
+
+    void OnTriggerEnter(Collider player)
+    {
+
+        //target is gonna disappear from the original place
+        item.SetActive(false);
+      
+
+        //reappear after a few sec at the target location
+        if (Time.deltaTime < spawntime)
+        {
+            item.transform.position = targetLocation;
+            item.SetActive(true);
+        }
+
+    }
+
+}
