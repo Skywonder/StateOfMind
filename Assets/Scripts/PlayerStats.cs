4ab6e6eb3a928d16efb9cb5b991e179ab42181cc
Skywonder <kuanpinc@ics.uci.edu>
2016-05-18 12:53:04 -0700

    Updates(Kuan)

---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerStats : MonoBehaviour {
+
+    public int Stresslevel;
+    private int maxStresslevel;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Stresslevel += 1; 
+        Debug.Log(Stresslevel);
+
+
+
+
+        if(Stresslevel == 100)
+        {
+            Debug.Log("The player fainted");
+        
+        }
+	}
+
+}
---
+++ Assets/Scripts/TransformTrigger.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TransformTrigger : MonoBehaviour {
+    //script intended to translate players to new position
+    // Use this for initialization
+    public GameObject item; //<-this is the item that will translate to target location
+    public GameObject targetLocation;
+    public int counter = 1;
+
+    // Update is called once per frame
+    void Update()
+    { 
+    }
+
+    void OnTriggerEnter(Collider player)
+    {
+        if (counter == 1)
+        { 
+            item.transform.position = targetLocation.transform.position;
+            counter -= 1;  
+        }
+        
+
+    }
+}
