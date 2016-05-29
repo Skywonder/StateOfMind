2aed35c265ec81d0930311c021a05289e78c3ca2
dlmerritt <dlmerrit@uci.edu>
2016-05-28 17:26:19 -0700

    Lots of New Things
    Sample monster for hallucination, script kinda working. Trigger zone
    works.
    Monster Vanishing after it spawns is a work in progress. Switched a few
    things around in the bathroom.

---
+++ Assets/Scripts/MonsterVanish.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonsterVanish : MonoBehaviour {
+    private Vector3 position;
+    private Vector3 curPos;
+	// Update is called once per frame
+	void Update () {
+        curPos = position;
+	    if (curPos != position) {
+            WaitForSeconds(2);
+            render.enabled = false;
+        }
+	}
+}
