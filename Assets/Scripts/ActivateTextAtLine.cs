959b65fef591849ab56d2cdc348aecd79a5ddb73
Skywonder <kuanpinc@ics.uci.edu>
2016-05-31 14:48:04 -0700

    Changes

---
+++ Assets/Scripts/ActivateTextAtLine.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActivateTextAtLine : MonoBehaviour {
+    public TextAsset theText;
+
+    public int startLine;
+    public int endLine;
+
+    public TextBoxManager theTextBox;
+    public bool destroyWhenActivated;
+
+	// Use this for initialization
+	void Start () {
+        theTextBox = FindObjectOfType<TextBoxManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Player")
+        {
+
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+
+                theTextBox.ReloadScript(theText);
+                theTextBox.currentLine = startLine;
+                theTextBox.endAtLine = endLine;
+
+
+                if (destroyWhenActivated)
+                {
+                    Destroy(gameObject);
+                }
+            }
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/TextBoxManager.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TextBoxManager : MonoBehaviour {
+    public GameObject textBox;
+    public Text theText;
+    public TextAsset textFile;
+    public string[] textLines;
+    public int currentLine;
+    public int endAtLine;
+    public bool isActive;
+    public PlayerStats player;
+    // Use this for initialization
+    void Start()
+    {
+        player = FindObjectOfType<PlayerStats>();
+        if (textFile != null)
+        {
+            textLines = (textFile.text.Split('\n'));
+        }
+        if (endAtLine == 0)
+        {
+            endAtLine = textLines.Length - 1;
+        }
+
+        if (isActive)
+        {
+            EnableTextBox();
+        }
+        else {
+            DisableTextBox();
+        }
+
+    }
+    void Update()
+    {
+        if (!isActive)
+        {
+            return;
+        }
+        theText.text = textLines[currentLine];
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            currentLine += 1;
+        }
+        if (currentLine > endAtLine)
+        {
+            DisableTextBox();
+        }
+
+    }
+
+    public void EnableTextBox()
+    {
+        textBox.SetActive(true);
+        Time.timeScale = 0.0f;
+    }
+
+    public void DisableTextBox()
+    {
+        textBox.SetActive(false);
+        Time.timeScale = 1.0f;
+    }
+
+    public void ReloadScript(TextAsset theText)
+    {
+        if (theText != null)
+        {
+            textLines = new string[1];
+            textLines = (theText.text.Split('\n'));
+        }
+    }
+
+
+}
