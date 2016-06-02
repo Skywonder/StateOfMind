14252d032197e317bd0a1738578947e06e69d301
dlmerritt <dlmerrit@uci.edu>
2016-06-01 21:37:37 -0700

    changes

--- Assets/Scripts/TriggerZone.cs
+++ Assets/Scripts/TriggerZone.cs
@@ -9,11 +9,13 @@ public class TriggerZone : MonoBehaviour {
     public float spawntime = 5f;
     public float despawntime = 2f;
     public bool istriggered = false;
+    public PlayerStats playerstress;
+    public GameObject player;
 
 
 	// Update is called once per frame
 	void Update () {
-	    
+
 	}
 
 
@@ -23,11 +25,14 @@ public class TriggerZone : MonoBehaviour {
 
         //target is gonna disappear from the original place
         item.SetActive(false);
-      
+
+        //increase stress level rate
+        playerstress = player.GetComponent<PlayerStats>();
 
         //reappear after a few sec at the target location
         if (istriggered == false)
         {
+            playerstress.stressRate = .75f;
             if (Time.deltaTime < spawntime)
             {
                 item.transform.position = targetLocation.transform.position;
@@ -44,5 +49,6 @@ public class TriggerZone : MonoBehaviour {
             item.SetActive(false);
             
         }
+       playerstress.stressRate = .01f;
     }
 }
